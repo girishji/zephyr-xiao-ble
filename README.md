fff806c364231bfe649c91df04ef8ba58b8c7e60
Girish Palya <girishji@gmail.com>
2023-01-12 01:41:30 +0100

    First commit
    A  .gitignore
    M  README.md
    A  app/CMakeLists.txt
    A  app/Kconfig
    A  app/app_version.h.in
    A  app/boards/xiao_ble.overlay
    A  app/dts/bindings/gpio-test.yaml
    A  app/prj.conf
    A  app/src/main.c
    A  app/west.yml

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
-# zephyr-gpio-xiao-ble
-Zephyr GPIO Usage Sample for Seeed Xiao BLE
+## Zephyr GPIO Usage Sample for Seeed Xiao BLE
+
+Zephyr uses device trees. It is not straightforward to transition from sample
+LED blinky app that Zephyr provides to manipulating GPIO pins. This repository
+contains the plumbing needed to get GPIO pins to work under Zephyr.
+
+### Installation
+
+Clone this repository. Follow the [Getting
+Started](https://docs.zephyrproject.org/3.2.0/develop/getting_started/index.html)
+guide and install the following:
+
+- Dependencies
+- west tool
+- Zephyr SDK
+
+Do not install Zephyr sources according to the Getting Started guide but follow the
+procedure below. 
+
+```
+# Create Zephyr workspace
+cd zephyr-gpio-xiao-ble
+west init -l app/
+
+# Fetch modules
+west update
+
+# Export Zephyr so cmake can find it
+west zephyr-export
+
+# Install Zephyr python dependencies
+pip3 install -r zephyr/scripts/requirements-base.txt
+```
+
+### Build
+
+```
+cd zephyr-gpio-xiao-ble
+mkdir -p build
+west build -b xiao_ble app
+```
+
+### Flash
+
+Double press the reset button located on the top left side of the board. The
+UF2 bootloader presents itself as mountable disk. Copy the firmware file named
+`zephyr.uf2` from the `build/zephyr` directory to the newly mounted disk.
---
+++ app/src/main.c
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2022 FMK (Girish Palya)
+ * SPDX-License-Identifier: MIT
+ */
+
+/*
+ * Bindings for seeed are in dts/bindings/gpio/seeed-xiao-header.yaml
+ * and boards/arm/xiao_ble/
+ */
+
+#include <zephyr/drivers/gpio.h>
+#include <zephyr/kernel.h>
+#include <zephyr/logging/log.h>
+
+#include "app_version.h"
+
+LOG_MODULE_REGISTER(main, CONFIG_APP_LOG_LEVEL);
+
+#define GPIO_NODE DT_NODELABEL(xiao_test)
+
+#if !DT_NODE_EXISTS(GPIO_NODE)
+#error "Overlay for gpio test is not properly defined."
+#endif
+
+void main(void) {
+
+    printk("Xiao BLE Test Application %s\n", APP_VERSION_STR);
+
+    const struct gpio_dt_spec D0 =
+        GPIO_DT_SPEC_GET_BY_IDX(GPIO_NODE, gpios, 0);
+    const struct gpio_dt_spec D1 =
+        GPIO_DT_SPEC_GET_BY_IDX(GPIO_NODE, gpios, 1);
+
+    if (!gpio_is_ready_dt(&D0) || !gpio_is_ready_dt(&D1)) {
+        LOG_ERR("GPIOs not ready");
+        return;
+    }
+
+    /* Configure pins. */
+    if ((gpio_pin_configure_dt(&D0, GPIO_OUTPUT_ACTIVE) < 0) ||
+        (gpio_pin_configure_dt(&D1, GPIO_OUTPUT_ACTIVE) < 0)) {
+        LOG_ERR("GPIOs configuration failed");
+        return;
+    }
+
+    while (1) {
+        /*
+        if (gpio_pin_set_dt(&D0, 1) < 0) {
+            LOG_ERR("GPIO set high failed");
+            return;
+        }
+        if (gpio_pin_set_dt(&D1, 1) < 0) {
+            LOG_ERR("GPIO set high failed");
+            return;
+        } 
+        */
+        k_sleep(K_MSEC(5000));
+    }
+}
